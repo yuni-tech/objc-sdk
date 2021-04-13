a5f17b2208f5d147159278b3b2eb29a73f6e3102
zeng.wenbin <git@git.uneed.com:mobile/yuni-ios.git>
2021-04-13 16:58:45 +0800

    fix: _Nullable

--- QiniuSDK/Storage/QNUploadDelegate.h
+++ QiniuSDK/Storage/QNUploadDelegate.h
@@ -17,10 +17,10 @@
 
 /// 将要上传的数据，提交给外部处理，处理完成之后返回数据并上传
 /// @param data 可提交给外部处理的chunk data
--(NSData * _Nonnull)QNWillUploadChunkData:(NSData * _Nonnull)data NS_SWIFT_NAME(qnWillUploadChunk(data:));
+-(NSData * _Nullable)QNWillUploadChunkData:(NSData * _Nullable)data NS_SWIFT_NAME(qnWillUploadChunk(data:));
 
 /// 一个文件上传完成之后回调
 /// @param handler 上传完成之后的回调
--(void)QNUploadDataDidFinish:(QNResponseInfo  * _Nullable)info key:(NSString * _Nonnull)key resp:(NSDictionary * _Nonnull)resp NS_SWIFT_NAME(qnUploadDidFinish(info:key:resp:));
+-(void)QNUploadDataDidFinish:(QNResponseInfo  * _Nullable)info key:(NSString * _Nullable)key resp:(NSDictionary * _Nullable)resp NS_SWIFT_NAME(qnUploadDidFinish(info:key:resp:));
 
 @end
